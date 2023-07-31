97c4776e8d15225bc64177808f3177fd5170d926
Rony <alfdo981@gmail.com>
2023-07-31 00:18:23 -0400

    test

--- server/middleware/jwt.js
+++ server/middleware/jwt.js
@@ -4,6 +4,8 @@ import createError from "../utils/createError.js";
 export const verifyToken = (req, res, next) => {
   const token = req.cookies.access_token;
 
+  console.log(token);
+
   if (!token) return next(createError(401, "You are not authenticated"));
 
   jwt.verify(token, process.env.JWT_KEY, async (error, payload) => {
