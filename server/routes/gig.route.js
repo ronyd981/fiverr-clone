e7fd2de2823a7f69d1e8cc482fd5bbe145c4b3af
Rony <alfdo981@gmail.com>
2024-02-26 16:54:54 -0400

    test

--- server/routes/gig.route.js
+++ server/routes/gig.route.js
@@ -13,5 +13,8 @@ router.post("/", verifyToken, createGig);
 router.delete("/:id", verifyToken, deleteGig);
 router.get("/", getAllGigs);
 router.get("/:id", getGig);
+router.get("/test", (req, res) => {
+  res.send("Hello world");
+});
 
 export default router;
